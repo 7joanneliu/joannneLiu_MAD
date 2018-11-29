9fc6ee7ce9e5f505adad4cdc517271593d700c49
Joanne <joanne.liu@colorado.edu>
2018-11-29 07:44:52 -0700

    lab7
    explicit + implicit intents

---
+++ lab7/app/src/main/java/com/example/joanneliu/lab7/forwarded.java
@@ -0,0 +1,14 @@
+package com.example.joanneliu.lab7;
+
+public class forwarded {
+        private String forwardedName="Joanne";
+        private String forwardedURL="http://creative.colorado.edu/~joli3145/FWD/index.html";
+
+    public String getForwardedName(){
+        return forwardedName;
+    }
+    public String getForwardedURL(){
+        return forwardedURL;
+    }
+
+}
